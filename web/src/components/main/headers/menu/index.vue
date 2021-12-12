969018a2aaae33b26fc1cc68b8cefe9b5fae25e7
a3510377 <70706886+a3510377@users.noreply.github.com>
2021-12-12 23:04:43 +0000

    upData:Emmm

--- web/src/components/main/headers/menu/index.vue
+++ web/src/components/main/headers/menu/index.vue
@@ -1,4 +1,20 @@
-<template></template>
+<template>
+  <div class="menu flex">
+    <div class="list flex">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>10</div>
+    </div>
+    <div class="openMenu"><div class="style" /></div>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -10,4 +26,34 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.menu {
+  .list {
+    > div {
+      padding: 0 10%;
+    }
+  }
+  .openMenu {
+    width: 30px;
+    height: 30px;
+    .style {
+      &,
+      &:after,
+      &:before {
+        content: "";
+        position: absolute;
+        width: 30px;
+        height: 3px;
+        background-color: #fff;
+        color: #fff;
+      }
+      &:before {
+        top: -10px;
+      }
+      &:after {
+        top: 10px;
+      }
+    }
+  }
+}
+</style>
