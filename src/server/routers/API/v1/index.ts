d4fe4bbf055244fc7d1178b07cbf845c12e59a90
a3510377 <70706886+a3510377@users.noreply.github.com>
2021-12-13 14:23:39 +0000

    upData:express 怪怪der

--- src/server/routers/API/v1/index.ts
+++ src/server/routers/API/v1/index.ts
@@ -1,45 +1,11 @@
+/* /v1 */
 import express from "express";
-import { getIp } from "@/utils/axios";
-import dbType from "@/db";
-import { makeId, makeToken } from "@/utils/uuid";
+import create from "./create";
+import mainError from "../main";
 
 const router = express.Router();
 
-router
-  .get("/", (req, res) => {
-    res.status(200).json({ v: 1 });
-  })
-  .get("/setup", async (req, res) => {
-    // url ex: /setup?ip=1.1.1.1
-    const ip: string = req.query.ip as string;
-    if (!ip) return res.status(400).send("無 ip 參數");
-    let ipData = await getIp(ip);
-    if (!ipData) return res.status(400).send("ip 錯誤");
-    let db = req.app.get("db") as dbType;
-    let checks_catch = (db.catch.api.checks[ip] = {
-      token: makeToken(makeId(0)),
-      timeOut: setTimeout(() => delete db.catch.api.checks[ip], 1e3 * 60 * 5),
-      ipData,
-    });
-    res.send(checks_catch.token);
-  })
-  .get("/check", async (req, res) => {
-    const ip: string = req.query.ip as string;
-    const token: string = req.query.token as string;
-    let db = req.app.get("db") as dbType;
-    let checks = db.catch.api.checks;
-    if (!ip || !token || !Object.keys(checks).includes(ip))
-      return res.status(400).send("參數錯誤");
-    let thisCatch = checks[ip];
-    if (thisCatch.token != token) return res.status(401).send("token 錯誤");
-    clearTimeout(thisCatch.timeOut);
-    res.send(
-      await db.addSite(
-        thisCatch.ipData.longitude,
-        thisCatch.ipData.latitude,
-        thisCatch.ipData.ip
-      )
-    );
-  });
+router.get("/", (req, res) => res.status(200).json({ v: 1 })).use(create);
+// .use(mainError({ notFound: "awa" }));
 
 export default router;
--- src/types/db.data.ts
+++ src/types/db.data.ts
@@ -24,3 +24,19 @@ export interface siteInfo extends mainReturn {
   Token: string;
   ID: string;
 }
+
+export interface dataMosquitos {
+  Time: string; // 時間( timestamp1~timestamp2 )
+  humidity: number; // 濕度( % )
+  Mosquitos: number; // 蚊子數量
+  Temperature: number; // 溫度( 度 )
+}
+
+// let ex: dataMosquitos[] = [
+//   {
+//     Time: "1639398628645~1639398637309",
+//     humidity: 70,
+//     Mosquitos: 1,
+//     Temperature: 31.1,
+//   },
+// ];
