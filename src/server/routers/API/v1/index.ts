213b6a3384d8077ce2b42c045087787c65e19564
a3510377 <70706886+a3510377@users.noreply.github.com>
2021-12-13 14:52:03 +0000

    upData:卒...

--- src/server/routers/API/v1/index.ts
+++ src/server/routers/API/v1/index.ts
@@ -1,11 +1,15 @@
 /* /v1 */
 import express from "express";
-import create from "./create";
-import mainError from "../main";
+import create from "./user/create";
+// import mainError from "../main";
+import postData from "./data/postData";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.status(200).json({ v: 1 })).use(create);
+router
+  .get("/", (req, res) => res.status(200).json({ v: 1 }))
+  .use(create)
+  .use(postData);
 // .use(mainError({ notFound: "awa" }));
 
 export default router;
