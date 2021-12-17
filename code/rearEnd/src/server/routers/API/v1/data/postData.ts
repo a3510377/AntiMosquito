f3db8484070f1a0af60bc91035908713ec774d8c
a3510377 <a102009102009@gmail.com>
2021-12-18 07:32:45 +0800

    Update postData.ts

--- code/rearEnd/src/server/routers/API/v1/data/postData.ts
+++ code/rearEnd/src/server/routers/API/v1/data/postData.ts
@@ -19,7 +19,9 @@ router
           delete info._id;
           let area = info.location.area;
           let _t = `${area.county} ${area.town} ${area.village}`;
-          data[_t] ||= [];
+          let county = (data[area.county] ||= {});
+          let town = (county ||= {});
+          town ||= [];
           data[_t].push(info);
         })
       )
