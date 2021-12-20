49c276669715b74913a86fa6fc59bccb231df517
a3510377 <70706886+a3510377@users.noreply.github.com>
2021-12-20 15:02:11 +0000

    upData:Update index.vue

---
+++ src/web/assets/index.aab717b4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.2b52152f.js";import{d as r}from"./vendor.9ff3a2a7.js";const n=r({data(){return{}}});function t(o,a,s,f,d,p){return null}var i=e(n,[["render",t]]);export{i as default};
