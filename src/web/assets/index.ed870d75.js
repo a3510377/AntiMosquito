b9fd21dc4b068b24ffd3bcaafac6b3c15dc36ab5
a3510377 <70706886+a3510377@users.noreply.github.com>
2021-12-23 10:12:28 +0000

    upData:use setup

--- src/web/assets/index.ed870d75.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.a2d8ace8.js";import{d as r}from"./vendor.645ecb24.js";const n=r({data(){return{}}});function t(o,a,s,d,c,p){return null}var i=e(n,[["render",t]]);export{i as default};
