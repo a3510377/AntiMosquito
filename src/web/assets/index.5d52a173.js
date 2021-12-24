b7c80c07309d0702ef987acbea3d4a51e2e08fba
a3510377 <70706886+a3510377@users.noreply.github.com>
2021-12-24 05:58:33 +0000

    upData:喵

--- src/web/assets/index.5d52a173.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.4b54381c.js";import{d as r}from"./vendor.3f617bf1.js";const n=r({data(){return{}}});function t(o,s,a,d,f,c){return null}var i=e(n,[["render",t]]);export{i as default};
--- web/src/components/main/map/index.vue
+++ web/src/components/main/map/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="map" ref="map" />
-  <button
-    class="t"
-    @click="() => layers.NS1Test.setVisible(!layers.NS1Test.getVisible())"
-  ></button>
   <div class="loading" ref="loading" />
 </template>
 
@@ -20,11 +16,14 @@ import { TopoJSON, GeoJSON } from "ol/format";
 import { fromLonLat } from "ol/proj";
 import { Geometry, Point } from "ol/geom";
 import { Tile as TileLayer } from "ol/layer";
+import { defaults as defaultControls } from "ol/control";
 import "ol/ol.css";
 
 import { apiUrl } from "@/config";
 import { ApiMainData } from "@/types/apiData";
 import { cFeature, countyStyle, townStyle, villageStyle } from "./util";
+import { baseControl, baseControlDiv } from "./controls";
+
 export default defineComponent({
   data() {
     return {
@@ -127,6 +126,7 @@ export default defineComponent({
       }),
     } as unknown as { [key: string]: layerVector<sourceVector<Geometry>> });
     let A_map = new Map({
+      controls: defaultControls(),
       target: map,
       layers: [
         new TileLayer({
@@ -140,9 +140,19 @@ export default defineComponent({
         layers.locationPoint,
       ],
       view: appView,
-      controls: defaults({ attribution: false }).extend([
-        new Attribution({ collapsible: false, collapsed: true }),
-      ]),
+    });
+    new baseControlDiv({
+      target: map,
+      control: [
+        new baseControl(
+          {
+            target: map,
+            text: `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="1.375em" viewBox="0 0 24 24" width="1.375em" fill="#fff"><rect fill="none" height="24" width="24"/><path d="M19.5,6c-1.31,0-2.37,1.01-2.48,2.3C15.14,7.8,14.18,6.5,12,6.5c-2.19,0-3.14,1.3-5.02,1.8C6.87,7.02,5.81,6,4.5,6 C3.12,6,2,7.12,2,8.5V9c0,6,3.6,7.81,6.52,7.98C9.53,17.62,10.72,18,12,18s2.47-0.38,3.48-1.02C18.4,16.81,22,15,22,9V8.5 C22,7.12,20.88,6,19.5,6z M3.5,9V8.5c0-0.55,0.45-1,1-1s1,0.45,1,1v3c0,1.28,0.38,2.47,1.01,3.48C4.99,14.27,3.5,12.65,3.5,9z M20.5,9c0,3.65-1.49,5.27-3.01,5.98c0.64-1.01,1.01-2.2,1.01-3.48v-3c0-0.55,0.45-1,1-1s1,0.45,1,1V9z M10.69,10.48 c-0.44,0.26-0.96,0.56-1.69,0.76V10.2c0.48-0.17,0.84-0.38,1.18-0.58C10.72,9.3,11.23,9,12,9s1.27,0.3,1.8,0.62 c0.34,0.2,0.71,0.42,1.2,0.59v1.04c-0.75-0.21-1.26-0.51-1.71-0.78C12.83,10.2,12.49,10,12,10C11.51,10,11.16,10.2,10.69,10.48z"/></svg>`,
+            title: "快篩院所",
+          },
+          () => layers.NS1Test.setVisible(!layers.NS1Test.getVisible())
+        ),
+      ],
     });
     A_map.on("singleclick", (e) => {
       /* 里 */
