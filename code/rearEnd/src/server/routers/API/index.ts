3581321508314373bca739fcab53e0c5fc199566
a3510377 <a102009102009@gmail.com>
2021-12-24 22:39:31 +0800

    喵

--- code/rearEnd/src/server/routers/API/index.ts
+++ code/rearEnd/src/server/routers/API/index.ts
@@ -1,5 +1,7 @@
 /* /api */
 import express from "express";
+import axios from "axios";
+
 import config from "@/config";
 import mainError from "./main";
 import v1 from "./v1";
@@ -7,6 +9,20 @@ import v1 from "./v1";
 const router = express.Router();
 
 router
+  .all("/pls", async (req, res) => {
+    if (!req.query.url) return res.status(400).json({});
+    axios({ url: req.query.url as string, method: "GET" })
+      .then((d) => {
+        for (let [key, value] of Object.entries(d.headers))
+          res.setHeader(key, value);
+        res.status(d.status || 200).json(d.data);
+      })
+      .catch((error) => {
+        res
+          .status((error.response && error.response.status) || 500)
+          .json({ error: error });
+      });
+  })
   .get("/", (req, res) => {
     res.status(200).json({
       __APIversion__: config.apiV,
