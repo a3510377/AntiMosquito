52ac326cfb6c4394f0ba470135a9f51be69f6854
猴子 <a102009102009@gmail.com>
2022-01-05 06:51:04 +0800

    add try

--- code/arduino/antimosquito/t.js
+++ code/arduino/antimosquito/t.js
@@ -1,15 +1,15 @@
 import net from "net";
 
 const client = new net.Socket();
-const HOST = "168.1.1.1";
+const HOST = "192.168.0.138";
+const token = "ppml5Juie2o8dLTm.1k1kk0g.YNrYQHHXirtl8CK1qIOD";
+const path = "/api/v1/nowData";
+const ip = "114.529.1.1";
 
 client.connect(3000, HOST, () => {
-  client.write(
-    "GET /api/v1/nowData?ip=1.1.1.1&time=999999&humidity=1&mosquitos=1&temperature=1 HTTP/1.1\r\n"
-  );
+  client.write("GET " + path + "/" + token + "/" + ip + "/1 HTTP/1.1\r\n");
   client.write(`Host: ${HOST}\r\n`);
   client.write("Content-Type: application/json\r\n");
-  client.write("authorization: awa\r\n");
   client.write("\r\n");
 });
 
