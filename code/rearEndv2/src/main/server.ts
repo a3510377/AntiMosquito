08f50bcaa807a29bfb6c6251da5c567637a7050b
a3510377 <a102009102009@gmail.com>
2022-03-10 23:03:40 +0800

    Update server.ts

--- code/rearEndv2/src/main/server.ts
+++ code/rearEndv2/src/main/server.ts
@@ -10,5 +10,13 @@ export class server {
   public app = express();
   public server: Server = createServer(this.app);
   public db: dbServer = new dbServer(this);
-  constructor() {}
+  /**創建用戶 */
+  public async createUser(name: string, description?: string) {
+    return await (
+      await this.db.model.UserModel.create({ name, description }).catch()
+    )
+      ?.save()
+      .catch(() => false);
+  }
+  public async findUser() {}
 }
