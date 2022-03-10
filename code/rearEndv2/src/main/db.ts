a06c613c32185b1415f0cbf8cf49fcd9f98c5da5
a3510377 <a102009102009@gmail.com>
2022-03-10 23:05:09 +0800

    打錯地方..

--- code/rearEndv2/src/main/db.ts
+++ code/rearEndv2/src/main/db.ts
@@ -6,4 +6,12 @@ import { server } from "./server";
 export class dbServer {
   public model = { DataModel, UserModel };
   constructor(public server: server) {}
+
+  /**創建用戶 */
+  public async createUser(name: string, description?: string) {
+    return await (await UserModel.create({ name, description }).catch())
+      ?.save()
+      .catch(() => false);
+  }
+  public async findUser() {}
 }
--- code/rearEndv2/src/main/server.ts
+++ code/rearEndv2/src/main/server.ts
@@ -10,13 +10,4 @@ export class server {
   public app = express();
   public server: Server = createServer(this.app);
   public db: dbServer = new dbServer(this);
-  /**創建用戶 */
-  public async createUser(name: string, description?: string) {
-    return await (
-      await this.db.model.UserModel.create({ name, description }).catch()
-    )
-      ?.save()
-      .catch(() => false);
-  }
-  public async findUser() {}
 }
