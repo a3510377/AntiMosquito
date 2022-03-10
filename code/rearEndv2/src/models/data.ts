45bc731cfc547522fe882bc7bbb2071d5f906b39
a3510377 <a102009102009@gmail.com>
2022-03-11 07:04:22 +0800

    Update data.ts

--- code/rearEndv2/src/models/data.ts
+++ code/rearEndv2/src/models/data.ts
@@ -1,7 +1,11 @@
 import { Document, Schema, model } from "mongoose";
 
-export interface dataType extends Document {}
+export interface dataType extends Document {
+  Location: { longitude: number; latitude: number };
+}
 
-export const guildSchema = new Schema<dataType>();
+export const guildSchema = new Schema<dataType>({
+  Location: { longitude: Number, latitude: Number },
+});
 
 export default model<dataType>("data", guildSchema);
