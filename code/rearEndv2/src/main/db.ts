9497a46b186199933b9c06deda3cdbe976d1478f
a3510377 <a102009102009@gmail.com>
2022-03-11 07:01:33 +0800

    Update db.ts

--- code/rearEndv2/src/main/db.ts
+++ code/rearEndv2/src/main/db.ts
@@ -1,6 +1,6 @@
-import { FilterQuery } from "mongoose";
+import { FilterQuery, AnyObject, AnyKeys } from "mongoose";
 
-import DataModel from "../models/data";
+import DataModel, { dataType } from "../models/data";
 import UserModel, { userType } from "../models/user";
 
 import { server } from "./server";
@@ -9,12 +9,19 @@ export class dbServer {
   public model = { DataModel, UserModel };
   constructor(public server: server) {}
   /**創建用戶 */
-  public async createUser(name: string, description?: string) {
-    return await (await UserModel.create({ name, description }).catch())
-      ?.save()
-      .catch(() => false);
+  public async createUser(data: AnyObject | AnyKeys<userType>) {
+    return await (await UserModel.create(data).catch())?.save().catch();
   }
+  /**尋找用戶 */
   public async findUser(find: FilterQuery<userType>) {
-    return await UserModel.findOne(find);
+    return await UserModel.findOne(find).catch();
+  }
+  /**創建資料 */
+  public async createData(data: AnyObject | AnyKeys<dataType>) {
+    return await (await DataModel.create(data).catch())?.save().catch();
+  }
+  /**尋找資料 */
+  public async findData(find: FilterQuery<dataType>) {
+    return await DataModel.findOne(find).catch();
   }
 }
