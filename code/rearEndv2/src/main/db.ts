1009b546362fc3d402b0d75a497016214b87fbaf
a3510377 <a102009102009@gmail.com>
2022-03-10 23:10:22 +0800

    Update db.ts

--- code/rearEndv2/src/main/db.ts
+++ code/rearEndv2/src/main/db.ts
@@ -1,17 +1,20 @@
+import { FilterQuery } from "mongoose";
+
 import DataModel from "../models/data";
-import UserModel from "../models/user";
+import UserModel, { userType } from "../models/user";
 
 import { server } from "./server";
 
 export class dbServer {
   public model = { DataModel, UserModel };
   constructor(public server: server) {}
-
   /**創建用戶 */
   public async createUser(name: string, description?: string) {
     return await (await UserModel.create({ name, description }).catch())
       ?.save()
       .catch(() => false);
   }
-  public async findUser() {}
+  public async findUser(find: FilterQuery<userType>) {
+    return await UserModel.findOne(find);
+  }
 }
