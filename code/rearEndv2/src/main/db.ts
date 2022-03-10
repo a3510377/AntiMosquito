568d00ee040b021706c0f545004b3466b3decb0a
a3510377 <a102009102009@gmail.com>
2022-03-10 22:56:22 +0800

    set db

--- code/rearEndv2/src/main/db.ts
+++ code/rearEndv2/src/main/db.ts
@@ -1,3 +1,12 @@
+import DataModel from "../models/data";
+import UserModel from "../models/user";
+
+import { server } from "./server";
+
 export class serverDb {
-  constructor() {}
+  public model = {
+    DataModel,
+    UserModel,
+  };
+  constructor(public server: server) {}
 }
