d5ab7e2d3da799908a48b957448fb9b489ef9833
a3510377 <a102009102009@gmail.com>
2022-03-12 22:16:39 +0800

    test

--- code/rearEndv2/src/main/opencv.ts
+++ code/rearEndv2/src/main/opencv.ts
@@ -1,1 +1,7 @@
 import cv from "opencv.js";
+import { createCanvas } from "canvas";
+
+let canvas = createCanvas(200, 200);
+let ctx = canvas.getContext("2d");
+
+let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
