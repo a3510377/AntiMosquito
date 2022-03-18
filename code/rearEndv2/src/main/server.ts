edb753c2fc0b0ec516a43673db7d44c68e447327
猴子 <70706886+a3510377@users.noreply.github.com>
2022-03-19 07:51:34 +0800

    掛載 伺服器

--- code/rearEndv2/src/main/server.ts
+++ code/rearEndv2/src/main/server.ts
@@ -2,11 +2,13 @@ import express, { Express } from "express";
 import { Server, createServer } from "http";
 import { config } from "dotenv";
 import mongoose from "mongoose";
+import logger from "morgan";
+import path from "path";
 
 import { dbServer } from "./db";
 import { wsServer } from "./gateway";
-
 import { checkPort } from "../utils/string";
+import routers from "../router";
 
 export let port: number = checkPort(process.env.PORT);
 
@@ -18,6 +20,33 @@ export class server {
   public db: dbServer = new dbServer(this);
   public ws: wsServer = new wsServer(this);
   public config = { db: { uri: process.env.dbUri } };
+  constructor() {
+    this.server
+      .on("error", (error: ErrnoException) => {
+        if (error.syscall !== "listen") throw error;
+
+        let bind = (typeof port === "string" ? "Pipe" : "Port") + port;
+
+        switch (error.code) {
+          case "EACCES":
+            console.error(`${bind} 需要更高全縣`);
+            this.server.listen(++port);
+            break;
+          case "EADDRINUSE":
+            console.error(`${bind} 已使用`);
+            this.server.listen(++port);
+            break;
+          default:
+            throw error;
+        }
+      })
+      .on("listening", async () => {
+        let addr = this.server.address();
+        let bind =
+          typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+        console.log(`is ready ${bind}`);
+      });
+  }
   /**start */
   public async start() {
     await mongoose
@@ -25,4 +54,30 @@ export class server {
       .then(() => console.log("資料庫連接完成"))
       .catch(() => console.log("資料庫連接錯誤"));
   }
+  public init() {
+    this.app
+      .set("view engine", "html")
+      .set("db", this.db)
+      .set("ws", this.ws)
+      .set("port", port)
+      .use(require("cors")())
+      .use(express.json())
+      .use(express.urlencoded({ extended: false }))
+      .use(logger("dev"))
+      .use("/", express.static(path.join(__dirname, "../web")))
+      .get("/uptimerobot", (_req, res) => {
+        res.status(200).send();
+        console.info("uptimerobot check");
+      })
+
+      .use(routers);
+  }
+}
+
+export interface ErrnoException extends Error {
+  errno?: number;
+  code?: string;
+  path?: string;
+  syscall?: string;
+  stack?: string;
 }
