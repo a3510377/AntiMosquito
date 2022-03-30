9f56d132c78518ae7542078ee61c737d896321e0
猴子 <a102009102009@gmail.com>
2022-03-30 16:49:58 +0800

    Update index.ts

--- code/rearEndv2/src/router/API/v1/index.ts
+++ code/rearEndv2/src/router/API/v1/index.ts
@@ -8,11 +8,12 @@ const router = express.Router();
 router
   .get("/", (_req, res) => res.status(200).json({ v: 1 }))
   .use("/postImg", postImg)
-  .post("/makeId", (req) => {
+  .post("/makeId", (req, res) => {
     let server: server = <server>req.app.get("main");
-    server.pin >= 9;
-    let id = `${+new Date()}${0}`;
+    server.pin = server.pin >= 9 ? 0 : server.pin + 1;
+    let id = `${+new Date()}${server.pin}`;
     server.db.createUser({ id });
+    res.send(id);
   });
 
 export default router;
