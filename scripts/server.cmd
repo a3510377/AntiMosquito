e1debe094dca572f493d9cb963d4ef267e8d493b
猴子 <a102009102009@gmail.com>
2022-04-20 17:26:29 +0800

    debug

--- scripts/server.cmd
+++ scripts/server.cmd
@@ -3,7 +3,7 @@
 if %ERRORLEVEL% neq 0 goto start
 
 
-::start
+:start
 git pull & cd .\code\rearEndv2 & npm install & npm run start
 
 goto start
--- scripts/web.cmd
+++ scripts/web.cmd
@@ -3,7 +3,7 @@
 if %ERRORLEVEL% neq 0 goto start
 
 
-::start
+:start
 git pull & cd .\code\web & npm install & npm run dev
 
 goto start
