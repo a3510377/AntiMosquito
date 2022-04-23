1e995954cb0b55cffe9746d12ec8d2037d51b54d
a3510377 <70706886+a3510377@users.noreply.github.com>
2022-04-23 16:08:30 +0000

    upData:ww

--- src/web/assets/config.1f9c29fe.js
+++
@@ -1,1 +0,0 @@
-const s={}.VITE_SSL===void 0?!0:{}.VITE_SSL,t="/",o={}.VITE_API_URL||"https://antimosquito.a102009102009.repl.co";export{o as a,s,t as w};
---
+++ src/web/assets/config.2f2ca7ea.js
@@ -0,0 +1,1 @@
+const t={}.VITE_API_URL||"https://antimosquito.a102009102009.repl.co";export{t as a};
